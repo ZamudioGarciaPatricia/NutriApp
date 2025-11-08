a30867d3761554b0d8b7810d6a76d61d57eff84b
ZamudioGarciaPatricia <24308060610116@cetis61.edu.mx>
2025-11-08 08:36:06 -0700

    a

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def inicio():
     return render_template('inicio.html')
 
 @app.route('/formulario')
